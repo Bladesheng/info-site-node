4c5436560d71e015873b250464ce10796533b769
Bladesheng <keadr23@gmail.com>
2022-11-07 17:16:00 +0100

    Add logger middleware

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ import path from "path";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Request Date: " + new Date());
+  console.log("Request IP: " + req.ip);
+  console.log("Request Method: " + req.method);
+  console.log("Request Path: " + req.path);
+  console.log("Request Protocol: " + req.protocol);
+  console.log();
+
+  next();
+});
+
 app.use(express.static("public"));
 
 // 404 handler
